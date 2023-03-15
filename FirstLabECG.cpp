e2bc21f1a95422deb9353bedf1c20af8099f02cd
H1Do <90624145+H1Do@users.noreply.github.com>
2023-03-15 18:22:34 +0500

    Уже треугольник (3 урок)

--- FirstLabECG.cpp
+++ FirstLabECG.cpp
@@ -13,7 +13,7 @@ static void RenderSceneCB()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // Указываем конвейеру, как воспринимать данные внутри буфера
 
-    glDrawArrays(GL_POINTS, 0, 1); // Запускаем функцию для отрисовки
+    glDrawArrays(GL_TRIANGLES, 0, 3); // Запускаем функцию для отрисовки
 
     glDisableVertexAttribArray(0); // Выключаем атрибуты вершины
     
@@ -27,8 +27,10 @@ static void InitializeGlutCallbacks()
 
 static void CreateVertexBuffer()
 {
-    Vector3f Vertices[1];
-    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
